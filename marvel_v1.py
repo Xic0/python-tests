2b00eb51c7e97c6645a74df3e0e28b4e2326d085
Xic0 <xicodax@gmail.com>
2014-08-21 05:46:38 +0100

    third commit

--- marvel_v1.py
+++ marvel_v1.py
@@ -19,15 +19,21 @@ class Character:
     evasion = 0
     points = 366
     _attributes = ['name', 'type', 'skills', 'health', 'stamina', 'attack', 'accuracy', 'defense', 'evasion', 'effects']
+
     def __init__ (self, name):
         assert self.valid_name(name)
         self.name = name
         Character.count += 1
         Character.chars.append(name)
+
+
+
     def getattribute(self, attribute):
         return eval("self." + attribute) if attribute in self._attributes else "### ERROR ###"
+
     def setattribute(self, attribute, ammount):
         eval("self." + attribute + " = " + ammount) if attribute in self._attributes else "### ERROR ###"
+
     def addPoints(self):
         accepted = ['health', 'stamina', 'attack', 'accuracy', 'defense', 'evasion']
         accepted_dict = dict(enumerate(accepted, start=1))
@@ -50,13 +56,14 @@ class Character:
                     print "You must enter an integer amount"
         self.setattribute(attribute, self.getattribute(attribute) + amount)
         self.points -= amount
+
     def __str__(self):
         return "\n".join("%s\t:\t%s"%(n, self.getattribute(n)) for n in self._attributes)
 
     @staticmethod
     def list ():
         """ Lists all characters defined by this class """
-        print("There is {} characters:".format(Character.count))
+        print("Total characters: {}".format(Character.count))
         for c in Character.chars:
             print "\t" + c
     @staticmethod
@@ -75,19 +82,41 @@ spidey.accuracy = 26
 spidey.evasion = 31
 spidey.effects = ['Great Responsability']
 spidey.skills = ['Web Shot', 'Spider-Sense', 'Web Slingshot', 'Web Swing']
-print str(spidey)
+#print str(spidey)
 #print Character.list(-1)
-print spidey.getattribute('health')
-hulk = Character('Hulk')
+#print spidey.getattribute('health')
+#hulk = Character('Hulk')
 
+#print Character.list()
 
-print Character.list()
-
-
-class effects:
-    """ Class doc """
+class Hero(Character):
+    """ Expands Character Class - Hero """
+    HEROES = ['Iron Man', 'Spider-Man', 'Hulk']
 
     def __init__ (self, name):
-        """ Class initialiser """
+        """ Function doc """
+        assert self.valid_name(name)
         self.name = name
-        self.description = ''
+        try:
+            assert name in self.HEROES
+            if name == "Iron Man":
+                self.type = "Blaster"
+                self.importAttributes([1,2,3,4,5,6])
+        except AssertionError:
+            print "ERROR: %s is an invalid Hero name." % name
+
+    def importAttributes (self, attributes):
+        """ Load Attributes """
+        if len(attributes) == 6:
+            for a in range(6):
+                #print type(attributes[a])
+                #Character.setattribute(Character._attributes[a+3], attributes[a])
+                #eval("self."+Character._attributes[a+3]+"=666")
+                super(self.setattribute(Character._attributes[a+3], attributes[a]))
+                print "setting {} with {}".format(Character._attributes[a+3], attributes[a])
+
+ironman = Hero("Iron Man")
+print str(ironman)
+
+#teste = [1,2,3,4,5,6]
+#ironman.importAttributes(teste)
